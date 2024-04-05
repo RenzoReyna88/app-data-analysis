4239304103b2041fadb22cf895bd876cedf95567
Renzo Reyna <reynarenzo.88@gmail.com>
2024-04-04 23:00:59 -0300

    Actualización de comoponentes de los gráficos

--- app_analyst.py
+++ app_analyst.py
@@ -1,53 +1,102 @@
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc, callback, Output, Input, dash_table
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
+from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
 
 
 
-
-edad_part = ('Entre 16 y 18 años', 'Entre 24 y 35 años', 'entre 35 y 45 años', 
-          'Entre 45 y 55 años', '+ 60 años')
-
-cant_part= [21, 37, 30, 15, 3]
-
-df= pd.DataFrame({"Rango de edades":edad_part, "Número de participantes":cant_part})
-
-app = Dash(__name__)
-
+# Inicialización de la app Dash
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server= app.server
 
+# Lectura de archivos CSV
+edades_participantes= pd.read_csv("datos/edades_participantes.csv", sep=',')
 
-colors_bar= ['#001F49', '#CB0000', '#006B37', '#5200F4', '#FF6100']
+prob_de_vivir_sarm= pd.read_csv("datos/prob_de_vivir_sarm.csv", sep=',')
 
+resp_sueldo= pd.read_csv("datos/sueldo.csv", sep=',')
 
+ddf= pd.read_csv("datos_encuestados.csv", sep=',')
+ddff= ddf.head(11)
 
+# Ploteo de las figuras
+# Figura n° 1
+fig_edades = px.bar(edades_participantes, x=edades_participantes['Rango de edades'], y=edades_participantes['Número de participantes'], 
+                    color='Rango de edades')
+fig_edades.update_layout(title=dict(
+                                        text='Números de participantes según su edad'),
+                                        plot_bgcolor='lightslategrey',
+                                        paper_bgcolor='lightslategrey',
+                                        font_color='white'
+                            )
 
-fig = px.bar(df, x=df['Rango de edades'], y=df['Número de participantes'], color='Rango de edades')
+# Figura n° 2
+fig_num_part = px.bar(resp_sueldo, x=resp_sueldo['Porcentajes respuestas'], y=resp_sueldo['Respuesta sobre el sueldo'], 
+                      color='Respuesta sobre el sueldo')
+fig_num_part.update_layout(title=dict(
+                                        text='Salario promedio'),
+                                        plot_bgcolor='lightslategrey',
+                                        paper_bgcolor='lightslategrey',
+                                        font_color='white'
+                            )
 
+# Figura n° 3
+fig_estudios = px.scatter(ddf, x=ddf['Nivel de estudios'], y=ddf['Profesión'], color='Nivel de estudios')
+fig_estudios.update_layout(title=dict(
+                                        text='Profesión frente a nivel de estudios'),
+                                        plot_bgcolor='lightslategrey',
+                                        paper_bgcolor='lightslategrey',
+                                        font_color='white',
+                            )
 
-app.layout= html.Div(children=[
-                               html.H1(children='Estudio económico en Sarmiento (Informe final)', style={'textAlign':'center'}),
+# Diseño de la aplicación (Interfaz)
+app.layout = dbc.Container([
 
-                               html.P(children='Aquí el Resumen de todo la investigación', 
-                                      style={'textAlign':'center'}),
+                            dbc.Row([
+                                    html.Div(children=[
+                                                        html.H3(children='Estudio económico en Sarmiento (Informe final)', 
+                                                                style={'textAlign':'center'}),
+                                                        html.P(children='Aquí el Resumen de todo la investigación', 
+                                                               style={'textAlign':'center'}),
 
-                                html.Br(),
+                                                        html.Br(),
+                                                        
+                                                        dcc.Graph(id='edades-graph', figure=fig_edades),
+                                            ])
+                            
+                            ]),
 
-                                dcc.Graph(id='example-graph',
-                                        figure=fig
-                                       )
-                              ]
-                    )
+                            html.Br(),
 
-fig.update_layout(title=dict(text='Número de participantes según su edad'),
-                  legend=dict(y=0.9,x=1.0, bgcolor="white"),
-                  plot_bgcolor='#d9d9d9',
-                  paper_bgcolor='#d9d9d9',
-                  font_color='#333333'
-                 )
+                            dbc.Row([
+                                    dbc.Col([
+                                            html.Div(children=[html.H3(children=['Respuestas de los participantes'])
+                                                               ]),
+                                
+                                            dash_table.DataTable(data=ddff.to_dict('records'), style_table={'overflowX': 'auto'}),
+                                    ],width=4),
 
+                                    dbc.Col([
+                                            dcc.Graph(id='estudios-graph', figure=fig_num_part),
+                                    ], width=8)
+                            ]),
 
+                            dbc.Row([
+                                    dbc.Col([
+                                        html.Div(children=[
+                                            html.H3(children='texto', style={'textAlign':'center'}),
+                                            html.P(children='texto', style={'textAlign':'center'}),
+                                            html.Br(),
+                                            dcc.Graph(id='profesion-graph', figure=fig_estudios)
+                                            ])
+                                    ])
+                            
+                            ]),
 
+            ])
 
+# Ejecución del programa, solo si el módulo o variable nombrada "app" es el módulo principal.
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(debug=True)
